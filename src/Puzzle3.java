bc6a9ba10b688edc19abe7dddb3a53c1cbaf45c7
Yannis Mouseck <yannis-mouseck@t-online.de>
2022-12-03 11:09:18 +0100

    Initial commit

---
+++ src/Puzzle3.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Puzzle3 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("Puzzle3.txt"));
+        int result = 0;
+        while(sc.hasNext()){
+            String s = sc.nextLine();
+            int length = s.length();
+
+            
+        }
+    }
+}
